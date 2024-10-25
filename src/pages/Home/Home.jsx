bf3f079c2bbef0198adefe363b5f596014bfd7f7
Md Akash Miah <miahmdakash55@gmail.com>
2024-10-25 16:43:43 +0600

    get ready to fight

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import Header from "../Shared/Header/Header";
 import Navbar from "../Shared/Navbar/Navbar";
+import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
+import RightSideNav from "../Shared/RightSideNav/RightSideNav";
 
 const Home = () => {
   return (
     <div>
       <Header></Header>
       <Navbar></Navbar>
-      <h1 className="text-3xl">Hello from home</h1>
+      <div className="grid md:grid-cols-4 gap-4">
+        <div className="border">
+          <LeftSideNav></LeftSideNav>
+        </div>
+        <div className="border md:col-span-2">
+          <h1 className="3xl">News Coming Soon</h1>
+        </div>
+        <div className="border">
+          <RightSideNav></RightSideNav>
+        </div>
+      </div>
     </div>
   );
 };

c5a6b23dfe2b69a6de56f87e3b39dae5a1a60940
Md Akash Miah <miahmdakash55@gmail.com>
2024-10-25 17:42:06 +0600

    next work with left and right side bar

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -43,7 +43,6 @@ const Navbar = () => {
             {nlink}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{nlink}</ul>
